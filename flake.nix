43a09dc43b36158679f418bf8e3cbc25ceabf893
Karol Baryła <karol.baryla@scylladb.com>
2023-07-07 15:24:15 +0200

    wip

--- flake.nix
+++ flake.nix
@@ -1,23 +1,26 @@
 {
   inputs = {
     nixpkgs.url = "nixpkgs";
-    scylla_ccm.url = "git+file:///home/karolbaryla/repos/scylla-ccm";
+    scylla_ccm.url = "github:Lorak-mmk/scylla-ccm/nix";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs = { self, nixpkgs, flake-utils, scylla_ccm }:
     let
       ld_library_path = pkgs: pkgs.lib.makeLibraryPath [ pkgs.stdenv.cc.libc pkgs.libxcrypt-legacy ];
+      # https://fzakaria.com/2020/11/12/debugging-a-jnr-ffi-bug-in-nix.html
       jdk_with_library_path = jdk: name: pkgs: pkgs.symlinkJoin {
         inherit name;
         paths = [ jdk ];
         buildInputs = [ pkgs.makeWrapper ];
         postBuild = ''
           wrapProgram $out/bin/java \
-            --set LD_LIBRARY_PATH ${ld_library_path pkgs}
+            --set LD_LIBRARY_PATH ${ld_library_path pkgs} \
+            --set JAVA_TOOL_OPTIONS -Djava.library.path=${ld_library_path pkgs}
           
           wrapProgram $out/bin/javac \
-            --set LD_LIBRARY_PATH ${ld_library_path pkgs}
+            --set LD_LIBRARY_PATH ${ld_library_path pkgs} \
+            --set JAVA_TOOL_OPTIONS -Djava.library.path=${ld_library_path pkgs}
         '';
       };
       prepare_mvn = jdk: bin_name: pkgs:
@@ -39,14 +42,12 @@
         mvn8 = prepare_mvn jdk8 "mvn8" pkgs;
         mvn11 = prepare_mvn jdk11 "mvn11" pkgs;
         mvn17 = prepare_mvn jdk17 "mvn17" pkgs;
-        ccm = pkgs.scylla_ccm;
+        ccm = pkgs.scylla_ccm pkgs.python311;
       in {
         devShell =
           pkgs.mkShell {
             buildInputs = [ pkgs.git mvn8 mvn11 mvn17 jdk11 ccm pkgs.poetry];
             shellHook = ''
-              # https://fzakaria.com/2020/11/12/debugging-a-jnr-ffi-bug-in-nix.html
-              export JAVA_TOOL_OPTIONS=-Djava.library.path=${ld_library_path pkgs}
               unset JAVA_HOME
             '';
           };
